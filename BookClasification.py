890ad75bceba2ffa453b712b2a5ff0bc8759d6f7
04crockettc <cc.crockett23@gmail.com>
2025-12-10 16:02:32 -0500

    fit data to linearSVC so it can be compared to linear regression

--- BookClasification.py
+++ BookClasification.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.svm import LinearSVC
 
 #Load the goodreads dataset
 data = pd.read_csv("goodreads_data.csv")
@@ -18,6 +19,7 @@ data["is_classic"] = data["Genres"].str.contains("classics", case = False, na = 
 data["Description"] = data["Description"].fillna("")
 data["Description"] = data["Description"].str.lower()
 
+
 #split the dataset into train and test
 x = data["Description"]
 y = data["is_classic"]
@@ -29,21 +31,38 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 #TF-IDF 
 #Convert a collection of raw documents to a matrix of TF-IDF features
-vectorizer = TfidfVectorizer()
+vectorizer = TfidfVectorizer(stop_words="english")
 X_train_tfidf = vectorizer.fit_transform(X_train)
 X_test_tfidf = vectorizer.transform(X_test)
-
-
 #print(X_train_tfidf.shape)
 #print(X_test_tfidf.shape)
 
+
+
 #Fit model using logistic regression
+logistic_X_train_tfidf = X_train_tfidf
+logistic_y_train = y_train
+logistic_y_test = y_test
+logistic_X_test_tfidf = X_test_tfidf
+
 model = LogisticRegression(max_iter= 500)
+model.fit(logistic_X_train_tfidf, logistic_y_train)
+
+#predict using test set ofr logistic regression
+logistic_y_pred = model.predict(logistic_X_test_tfidf)
+
+
+# fit model to linear svc
+model = LinearSVC()
 model.fit(X_train_tfidf, y_train)
 
-# predict using the test set
+# predict linear svc using the test set
 y_pred = model.predict(X_test_tfidf)
 
+
 # evalutate performance
-print("Test Accuracy:", accuracy_score(y_test, y_pred), "\n")
+print("Test Accuracy for Logistic Regression:", accuracy_score(logistic_y_test, logistic_y_pred), "\n")
+print(classification_report(logistic_y_test, logistic_y_pred))
+
+print("Test Accuracy for LinearSVC:", accuracy_score(y_test, y_pred), "\n")
 print(classification_report(y_test, y_pred))
