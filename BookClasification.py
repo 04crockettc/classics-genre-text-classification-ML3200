adab615fd69dd2baa6f66454e8427e9ea4886dcd
04crockettc <cc.crockett23@gmail.com>
2025-12-10 14:57:22 -0500

    finish preprocessing steps

--- BookClasification.py
+++ BookClasification.py
@@ -1,19 +1,28 @@
 import numpy as py
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 
 #Load the goodreads dataset
 data = pd.read_csv("goodreads_data.csv")
 #print(data.head())
 
 # features in dataset = [number, book, author, desription,
-#  genres, avgRating, numRating, URL]
+# genres, avgRating, numRating, URL]
+# creates a binary target. 
 data["is_classic"] = data["Genres"].str.contains("classics", case = False, na = False).astype(int)
 
-x = data[['Description']]
-y = data['is_classic']
+
+# clean the text: handle missing discriptions, change strings to lower case, and remove puncutation
+data["Description"] = data["Description"].fillna("")
+data["Description"] = data["Description"].str.lower()
 
 #split the dataset into train and test
-from sklearn.model_selection import train_test_split
+x = data["'Description"]
+y = data['is_classic']
+
 
 X_train, X_test, y_train, y_test = train_test_split(
     x, y, test_size=0.3, random_state = 42
@@ -21,18 +30,15 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 #TF-IDF 
 #Convert a collection of raw documents to a matrix of TF-IDF features
-from sklearn.feature_extraction.text import TfidfVectorizer
-
 vectorizer = TfidfVectorizer()
 X_train_tfidf = vectorizer.fit_transform(X_train['Description'].fillna(''))
-X_test_tfidf = vectorizer.fit_transform(X_test['Description'].fillna(''))
+X_test_tfidf = vectorizer.transform(X_test['Description'].fillna(''))
 vectorizer.get_feature_names_out()
 
 #print(X_train_tfidf.shape)
 #print(X_test_tfidf.shape)
 
 
-#finish preprocessing
 #fit the model on training data
 #test the model
 #evaluate the results
