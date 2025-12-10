cf8bd7675a0597e727bb2c709c5640edec7abd1a
04crockettc <cc.crockett23@gmail.com>
2025-12-10 15:15:26 -0500

    Fit to Logistic Regression

--- BookClasification.py
+++ BookClasification.py
@@ -7,10 +7,9 @@ from sklearn.metrics import accuracy_score, classification_report
 
 #Load the goodreads dataset
 data = pd.read_csv("goodreads_data.csv")
-#print(data.head())
-
 # features in dataset = [number, book, author, desription,
 # genres, avgRating, numRating, URL]
+
 # creates a binary target. 
 data["is_classic"] = data["Genres"].str.contains("classics", case = False, na = False).astype(int)
 
@@ -20,8 +19,8 @@ data["Description"] = data["Description"].fillna("")
 data["Description"] = data["Description"].str.lower()
 
 #split the dataset into train and test
-x = data["'Description"]
-y = data['is_classic']
+x = data["Description"]
+y = data["is_classic"]
 
 
 X_train, X_test, y_train, y_test = train_test_split(
@@ -31,14 +30,20 @@ X_train, X_test, y_train, y_test = train_test_split(
 #TF-IDF 
 #Convert a collection of raw documents to a matrix of TF-IDF features
 vectorizer = TfidfVectorizer()
-X_train_tfidf = vectorizer.fit_transform(X_train['Description'].fillna(''))
-X_test_tfidf = vectorizer.transform(X_test['Description'].fillna(''))
-vectorizer.get_feature_names_out()
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
+
 
 #print(X_train_tfidf.shape)
 #print(X_test_tfidf.shape)
 
+#Fit model using logistic regression
+model = LogisticRegression(max_iter= 500)
+model.fit(X_train_tfidf, y_train)
 
-#fit the model on training data
-#test the model
-#evaluate the results
+# predict using the test set
+y_pred = model.predict(X_test_tfidf)
+
+# evalutate performance
+print("Test Accuracy:", accuracy_score(y_test, y_pred), "\n")
+print(classification_report(y_test, y_pred))
